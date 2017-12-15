5a87d12b8471aa1ed08c5346fc1bf029ecbb04d9
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-15 22:12:02 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,5 +8,7 @@ exports.run = (client) => {
   const channel = client.channels.get('308278703283240960');
   if (channel) channel.send('Yuga is now online!');
   setTimeout(function() {
-    client.channels.get('308278703283240960').send(`Yuga's recent pings are: ${client.pings}, with an average of ${client.ping}, API response time for Discord.`)
+    client.channels.get('308278703283240960').send(`Yuga's recent pings are: ${client.pings}, with an average of ${client.ping}, API response time for Discord.`),
+      1200000
+  })
 }
