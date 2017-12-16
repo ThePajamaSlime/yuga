88fe24e86730041bb5920b20a159f0920af23de4
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-16 23:07:58 +0000

    Woops I almost forgot something

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,7 @@
 exports.run = (client, member) => {
     let guild = member.guild;
+
     const channel = member.guild.channels.find('name', 'yuga-goodbye');
     if (!channel) return;
-    channel.send(`It's a shame to see you go, ${member.username}...`)
+    channel.send(`It's a shame to see you go, ${member.user.username}...`)
 }
