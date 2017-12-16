ef4800a6f8959d6e4ecae1fcbd824501787f1a28
Striker <zzstrikerzzyt@gmail.com>
2017-12-16 18:20:01 +0000

    Allowed restarters/management team to make Yuga leave servers.

--- commands/leave.js
+++ commands/leave.js
@@ -1,6 +1,7 @@
+const ids = require('../db/db.json').ids;
+
 exports.run = (client, msg, args) => {
-    const striker = '215509157837537280';
-    if (msg.author.id != striker) return;
-    const guild = args.join(' ');
-    client.guilds.get(guild).leave();
+   if(ids.indexOf(msg.author.id)) {
+       client.guilds.get(args.join(' ')).leave();
+   }
 };
