e4bb4dc2bc9354e10f90aae8084d117d785e994b
Striker <zzstrikerzzyt@gmail.com>
2017-12-16 15:38:40 +0000

    So apparently this needed to be changed.

--- yuga.js
+++ yuga.js
@@ -5,16 +5,6 @@ const client = new Discord.Client();
 const fs = require('fs');
 
 const color = require('./db/db.json').color;
-function error(err) {
-  const errorembed = new Discord.MessageEmbed()
-    .setColor(color)
-    .setTitle('New Error Caught!')
-    .setTimestamp()
-    .setDescription(`\`\`\`xl\n${err.stack}\`\`\``);
-  client.channels.get('308278703283240960').send('<@215509157837537280>', {
-    embed: errorembed
-  });
-}
 
 //Event Handler
 fs.readdir('./events/', (err, files) => {
@@ -27,6 +17,17 @@ fs.readdir('./events/', (err, files) => {
 });
 
 client.login(process.env.TOKEN);
+
+function error(err) {
+  const errorembed = new Discord.MessageEmbed()
+    .setColor(color)
+    .setTitle('New Error Caught!')
+    .setTimestamp()
+    .setDescription(`\`\`\`xl\n${err.stack}\`\`\``);
+  client.channels.get('385485532458778626').send({
+    embed: errorembed
+  });
+}
 
 process.on('uncaughtException', (err) => {
   error(err);
