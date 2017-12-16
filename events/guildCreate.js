6a6a188487d3bf536165a942b050cc69a87d338f
Striker <zzstrikerzzyt@gmail.com>
2017-12-16 11:19:11 +0000

    Update guildCreate and guildDelete

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,54 +1,52 @@
 const Discord = require('discord.js');
 
-exports.run = (client, guild) => {
+exports.run = async(client, guild) => {
 
     guild = client.guilds.get(guild.id);
-    guild.createChannel('yuga-info').then(channel => {
-        channel.createInvite({
-            maxAge: 0
-        }).then(realInvite => {
-
-            const serveradded = new Discord.MessageEmbed()
-                .setAuthor('Yuga')
-                .setColor('#08ff00')
-                .setTitle('New server added!')
-                .setThumbnail(client.user.avatarURL())
-                .setDescription('Yuga has been added to a new server!\nThe server info will be displayed below.')
-                .addField('Server info', `Name: ${guild.name}\nID: ${guild.id}\nMade: ${guild.createdAt}\nOwner: ${guild.owner.user.tag} (ID: ${guild.ownerID})\nRegion: ${guild.region}\nRoles: ${guild.roles.size}\nVerification Level: ${guild.verificationLevel}\nMembers: ${guild.members.size} \nInvite link: ${realInvite}`)
-                .setTimestamp();
-
-            const channelwelcome = new Discord.MessageEmbed()
-                .setAuthor('Yuga')
-                .setThumbnail(client.user.avatarURL())
-                .setColor('#ff4500')
-                .addField('Thanks for adding Yuga!', 'Hi! I\'m Yuga, thanks for adding me.\nI have many features, and if you wish to know them use "y!help" to get a list of all the amazing stuff I can do!\nThanks for listening, and I hope you enjoy using Yuga!')
-                .addField('Need to contact us?', 'You can always join the official server and ask for help there!\nWe are English speaking, but we can speak some foreign languages too.\nJoin here: https://discord.gg/vJBrsY6')
-                .setTimestamp();
-
-            const ownerwelcome = new Discord.MessageEmbed()
-                .setAuthor('Yuga')
-                .setThumbnail(client.user.avatarURL())
-                .setColor('#ff4500')
-                .addField('Thanks for adding Yuga!', 'Hi! I\'m Yuga, thanks for adding me.\nI have many features, and if you wish to know them use "y!help" to get a list of all the amazing stuff I can do!\nThanks for listening, and I hope you enjoy using Yuga!')
-                .addField('Need to contact us?', 'You can always join the official server and ask for help there!\nWe are English speaking, but we can speak some foreign languages too.\nJoin here: https://discord.gg/vJBrsY6')
-                .setTimestamp();
-
-            client.user.setActivity(`for y!help | ${client.guilds.size} servers`, {
-                type: 'WATCHING'
-            });
-
-            client.channels.get('308649578318528513').send({
-                embed: serveradded
-            });
-            guild.owner.send({
-                embed: ownerwelcome
-            });
-
-            channel.send({
-                embed: channelwelcome
-            });
-
-            channel.send('Yuga requires certain channels to function. To know more, run y!config');
-        });
+    const channel = await guild.createChannel('yuga-info')
+    const invite = await channel.createInvite({
+        maxAge: 0
     });
+
+    const serveradded = new Discord.MessageEmbed()
+        .setAuthor('Yuga')
+        .setColor('#08ff00')
+        .setTitle('New server added!')
+        .setThumbnail(client.user.avatarURL())
+        .setDescription('Yuga has been added to a new server!\nThe server info will be displayed below.')
+        .addField('Server info', `Name: ${guild.name}\nID: ${guild.id}\nMade: ${guild.createdAt}\nOwner: ${guild.owner.user.tag} (ID: ${guild.ownerID})\nRegion: ${guild.region}\nRoles: ${guild.roles.size}\nVerification Level: ${guild.verificationLevel}\nMembers: ${guild.members.size} \nInvite link: ${invite}`)
+        .setTimestamp();
+
+    const channelwelcome = new Discord.MessageEmbed()
+        .setAuthor('Yuga')
+        .setThumbnail(client.user.avatarURL())
+        .setColor('#ff4500')
+        .addField('Thanks for adding Yuga!', 'Hi! I\'m Yuga, thanks for adding me.\nI have many features, and if you wish to know them use "y!help" to get a list of all the amazing stuff I can do!\nThanks for listening, and I hope you enjoy using Yuga!')
+        .addField('Need to contact us?', 'You can always join the official server and ask for help there!\nWe are English speaking, but we can speak some foreign languages too.\nJoin here: https://discord.gg/vJBrsY6')
+        .setTimestamp();
+
+    const ownerwelcome = new Discord.MessageEmbed()
+        .setAuthor('Yuga')
+        .setThumbnail(client.user.avatarURL())
+        .setColor('#ff4500')
+        .addField('Thanks for adding Yuga!', 'Hi! I\'m Yuga, thanks for adding me.\nI have many features, and if you wish to know them use "y!help" to get a list of all the amazing stuff I can do!\nThanks for listening, and I hope you enjoy using Yuga!')
+        .addField('Need to contact us?', 'You can always join the official server and ask for help there!\nWe are English speaking, but we can speak some foreign languages too.\nJoin here: https://discord.gg/vJBrsY6')
+        .setTimestamp();
+
+    client.user.setActivity(`for y!help | ${client.guilds.size} servers`, {
+        type: 'WATCHING'
+    });
+
+    client.channels.get('308649578318528513').send({
+        embed: serveradded
+    });
+    guild.owner.send({
+        embed: ownerwelcome
+    });
+
+    channel.send({
+        embed: channelwelcome
+    });
+
+    channel.send('Yuga requires certain channels to function. To know more, run y!config');
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,8 +1,7 @@
-exports.run = (client, guild) => {
-  guild.owner.send('Hi! We see you kicked our bot. Could you explain briefly to Striker#7250 why, and we can consider improvements and such. Thanks!');
-  client.user.setActivity(`for y!help | ${client.guilds.size} servers`, {
+exports.run = async (client, guild) => {
+  await client.user.setActivity(`for y!help | ${client.guilds.size} servers`, {
     type: 'WATCHING'
   });
-
+  guild.owner.send('Hi! We see you kicked our bot. Could you explain briefly to Striker#7250 why, and we can consider improvements and such. Thanks!');
   client.channels.get('308649578318528513').send(`Kick from: ${guild.name}\nOwner: ${guild.owner.user.username}#${guild.owner.user.discriminator} (ID: ${guild.owner.id})`);
 };
