013cfdbcc554dde87541bbd45a9919f31da88e44
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-16 23:04:18 +0000

    Making it more mobile friendly, by not mentioning the users and instead saying their name

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,5 +2,5 @@ exports.run = (client, member) => {
     let guild = member.guild;
     const channel = member.guild.channels.find('name', 'yuga-goodbye');
     if (!channel) return;
-    channel.send(`It's a shame to see you go, ${member}...`)
+    channel.send(`It's a shame to see you go, ${member.username}...`)
 }
