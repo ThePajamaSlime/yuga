833c65c175e705938aa1a53cd05d8af854ff955c
Striker <zzstrikerzzyt@gmail.com>
2017-12-20 20:23:42 +0000

    Fix.

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -3,7 +3,9 @@ const db = require('quick.db');
 exports.run = (client, msg, args) => {
     const prefix = args.join(' ');
     if (!msg.author.id == msg.guild.owner.user.id) return;
-    db.updateText(msg.guild.id, prefix).then(p => {
-        msg.channel.send(`Updated prefix for this server to: ${p.text}`);
-    });
+    else {
+        db.updateText(msg.guild.id, prefix).then(p => {
+            msg.channel.send(`Updated prefix for this server to: ${p.text}`);
+        });
+    }
 };
