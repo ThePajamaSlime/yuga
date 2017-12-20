e33bcb979837da0ae82fa3c965e5416e50200677
Striker <zzstrikerzzyt@gmail.com>
2017-12-20 19:00:50 +0000

    Update

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,7 +1,7 @@
-const db = require('quick.db')
+const db = require('quick.db');
 
-exports.run = (client, msg, args) => {
+exports.run = (client, msg) => {
     db.fetchObject(msg.guild.id).then(prefix => {
-        msg.reply(`Prefix for this server is: ${prefix}`)
-    })
-}
+        msg.reply(`Prefix for this server is: ${prefix.text}`);
+    });
+};
--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,9 +1,9 @@
 const db = require('quick.db');
 
 exports.run = (client, msg, args) => {
-    const prefix = args.join(' ')
+    const prefix = args.join(' ');
 
     db.updateText(msg.guild.id, prefix).then(p => {
-        msg.channel.send(`Updated prefix for this server to: ${prefix}`)
-    })
-}
+        msg.channel.send(`Updated prefix for this server to: ${p.text}`);
+    });
+};
