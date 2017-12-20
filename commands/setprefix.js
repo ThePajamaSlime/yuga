1f660c2659ea7ba7cf52e4802a1edb7b186358f9
Striker <zzstrikerzzyt@gmail.com>
2017-12-20 18:40:09 +0000

    Command to set prefix for this guild

---
+++ commands/setprefix.js
@@ -0,0 +1,9 @@
+const db = require('quick.db');
+
+exports.run = (client, msg, args) => {
+    const prefix = args.join(' ')
+
+    db.updateText(msg.guild.id, prefix).then(p => {
+        msg.channel.send(`Updated prefix for this server to: ${prefix}`)
+    })
+}
