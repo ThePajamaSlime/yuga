4fe8e742171d6b494c71002924ff1336ab9b9666
Striker <zzstrikerzzyt@gmail.com>
2017-12-20 18:37:59 +0000

    Added appropriate command handler for checking prefixes

--- events/message.js
+++ events/message.js
@@ -1,8 +1,9 @@
 const prefix = 'y!';
 const Discord = require('discord.js');
 const color = require('../db/db.json').color;
+const db = require('quick.db')
 
-exports.run = async (client, msg) => {
+exports.run = async(client, msg) => {
     //Custom error catcher function
     function error(err) {
         const errorembed = new Discord.MessageEmbed()
@@ -16,6 +17,7 @@ exports.run = async (client, msg) => {
     }
     let args = msg.content.split(' ').slice(1);
     let command = msg.content.split(' ')[0];
+
     //Checking if it's a bot speaking & ignores it
     if (msg.author.bot) return;
 
@@ -27,6 +29,7 @@ exports.run = async (client, msg) => {
         msg.channel.send('You cannot use this command in DM, try sending this in a server.');
         //End
     }
+
     //Prefix Checker #1: Command Only
     if (msg.content.startsWith(prefix)) {
         command = command.slice(prefix.length);
@@ -100,6 +103,40 @@ exports.run = async (client, msg) => {
             embed: log
         }).then(msg.channel.stopTyping());
         //Logger
+    }
+
+    p = await db.fetchObject(msg.guild.id) 
+    if (!p) return;
+    
+    //Command Handler #5: Custom Prefixes
+    if (msg.content.startsWith(p.text)) {
+        command = command.slice(p.text.length)
+        console.log('Command running, Handler: 5');
+        msg.channel.startTyping();
+        const log = new Discord.MessageEmbed()
+            .setTitle('**__LOG__**')
+            .setColor(color)
+            .addField('User', `${msg.author.tag} ID: ${msg.author.id}`)
+            .addField('Command', `${msg.content}`)
+            .addField('Server', `${msg.guild.name} ID: ${msg.guild.id}`)
+            .setTimestamp()
+            .setThumbnail(client.user.avatarURL());
+        //Running Commands
+        try {
+            const commandFile = require(`../commands/${command}.js`);
+            commandFile.run(client, msg, args);
+        } catch (err) {
+            msg.reply(`Command execution failed!\n Error: ${err.message}\nCheck spelling of command, edit your message if you can.\nIf the error seems unusual, message @Striker#7250, or join the server and ask for help.\nPlease, post your error so we know what we're dealing with here :)`);
+            error(err);
+            msg.channel.stopTyping();
+        }
+        //End Running Commands
+
+        //Logger
+        client.channels.get('308545302615293953').send({
+            embed: log
+        }).then(msg.channel.stopTyping());
+        //Logger    
     } else return;
     //Code to do nothing if there is no prefix. All other messages are ignored thus.
 };
