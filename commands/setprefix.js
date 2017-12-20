d4135bcac41bc28f9c4f37495387692d68764a22
Striker <zzstrikerzzyt@gmail.com>
2017-12-20 20:02:02 +0000

    Fixed some logic

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -2,7 +2,7 @@ const db = require('quick.db');
 
 exports.run = (client, msg, args) => {
     const prefix = args.join(' ');
-    if (!msg.author == msg.guild.owner) return;
+    if (!msg.author.id == msg.guild.owner.user.id) return;
     db.updateText(msg.guild.id, prefix).then(p => {
         msg.channel.send(`Updated prefix for this server to: ${p.text}`);
     });
